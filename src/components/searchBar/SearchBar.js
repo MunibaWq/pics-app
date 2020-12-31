dda44f9dddedfb62a6f9bb702195189e8d19329d
Muniba Waqar <muniba.wq@gmail.com>
2020-12-31 16:53:01 -0700

    changing to class to use setState

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,22 +1,24 @@
 import React from "react";
 
-const SearchBar = () => {
+// const SearchBar = () => {
+class SearchBar extends React.Component {
   state = { term: "" };
-
-  return (
-    <div className=" ui segment ">
-      <form className=" ui form">
-        <div className="field">
-          <label>User Input</label>
-          <input
-            type="text"
-            value={this.state.term}
-            onChange={(e) => this.setState({ term: e.target.value })}
-          />
-        </div>
-      </form>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className=" ui segment ">
+        <form className=" ui form">
+          <div className="field">
+            <label>User Input</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
