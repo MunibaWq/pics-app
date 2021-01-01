25e6d2377592cd86ce6b7de6e9d38c6d33cddb0f
Muniba Waqar <muniba.wq@gmail.com>
2020-12-31 18:56:42 -0700

    adding functionality to the search feature in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import React, { Component } from "react";
 import "./App.css";
 import SearchBar from "./components/searchBar/SearchBar";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="ui container">
-        <SearchBar />
+class App extends Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="ui container">
+          <SearchBar onSubmit={this.onSearchSubmit} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
