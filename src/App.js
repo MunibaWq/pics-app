d2e33c45d374ad771b8a6a0e8eec4a8690a058ec
Muniba Waqar <muniba.wq@gmail.com>
2021-01-01 16:03:35 -0700

    fetching data from an api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
 import SearchBar from "./components/searchBar/SearchBar";
 
 class App extends Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: process.env.REACT_APP_UNSPLASH_API_KEY,
+      },
+    });
   }
 
   render() {
