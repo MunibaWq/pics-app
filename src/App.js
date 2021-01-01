e89524de5f49781a62afa1679eb63f9f5ddeb32b
Muniba Waqar <muniba.wq@gmail.com>
2021-01-01 16:12:56 -0700

    cl some results to make sure correct info is being fetched

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import SearchBar from "./components/searchBar/SearchBar";
 
 class App extends Component {
   onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization: process.env.REACT_APP_UNSPLASH_API_KEY,
-      },
-    });
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization: process.env.REACT_APP_UNSPLASH_API_KEY,
+        },
+      })
+      // .then((response) => {
+      //   console.log(response);
+      // });
+      .then((response) => {
+        console.log(response.data.results);
+      });
   }
 
   render() {
