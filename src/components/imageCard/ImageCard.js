b082d2216ee06c45ada01ec9ea83c4b5ae898e4a
Muniba Waqar <muniba.wq@gmail.com>
2021-01-02 13:43:03 -0700

    accessing the DOM with refs - useRef

--- src/components/imageCard/ImageCard.js
+++ src/components/imageCard/ImageCard.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const ImageCard = (props) => {
   const { description, urls } = props.image;
+  const imageRef = React.useRef();
+
+  useEffect(() => {
+    console.log(imageRef.current);
+    console.log(imageRef.current.clientHeight);
+    //   effect
+    //   return () => {
+    //       cleanup
+    //   }
+  });
+
   return (
     <div>
-      <img alt={description} src={urls.regular} />
+      <img ref={imageRef} alt={description} src={urls.regular} />
     </div>
   );
 };
