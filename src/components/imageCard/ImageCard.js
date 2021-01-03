8e6b4798129b26ec45cc2c3c3c4914ec28fc2dd7
Muniba Waqar <muniba.wq@gmail.com>
2021-01-03 14:18:51 -0700

    finish styling

--- src/components/imageCard/ImageCard.js
+++ src/components/imageCard/ImageCard.js
@@ -1,25 +1,28 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const ImageCard = (props) => {
   const { description, urls } = props.image;
   const imageRef = useRef();
+  const [spans, setSpans] = useState(0);
 
   useEffect(() => {
     console.log(imageRef.current);
     console.log(imageRef.current.clientHeight);
-    imageRef.current.addEventListener("load", setSpans);
+    imageRef.current.addEventListener("load", setSpan);
     //   effect
     //   return () => {
     //       cleanup
     //   }
   });
 
-  const setSpans = () => {
-    console.log("setSpans " + imageRef.current.clientHeight);
+  const setSpan = () => {
+    const height = imageRef.current.clientHeight;
+    const span = Math.ceil(height / 10);
+    setSpans(span);
   };
 
   return (
-    <div>
+    <div style={{ gridRowEnd: `span ${spans}` }}>
       <img ref={imageRef} alt={description} src={urls.regular} />
     </div>
   );
